4f5e6645a607cb4bb4be4673d3eb9668a734abf4
Juwel Rana <juwel.jem@gmail.com>
2024-12-21 09:37:01 +0600

    added

---
+++ ema-john-pagination-server-starter/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.PASSWORD}@cluster0.hjkzu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const productCollection = client.db("emaJohnDB").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/number-of-products", async (req, res) => {
+      const totalItems = await productCollection.estimatedDocumentCount();
+      res.send({ totalItems });
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("john is busy shopping");
+});
+
+app.listen(port, () => {
+  console.log(`ema john server is running on port: ${port}`);
+});
